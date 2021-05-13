257cebff7368e9b9174c4f7708da01249dff81d3
Eliascm17 <Elias.cmoreno17@gmail.com>
2021-05-13 16:09:23 -0500

    edited footer with two more links

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,7 @@ const ExternalLink = ({ children, href, alt, newTab = true }) => {
 };
 
 const Footer = () => (
-  <footer className="flex flex-col w-full items-start justify-center max-w-5xl mx-auto space-y-4 mb-12 mt-8">
+  <footer className="flex flex-col w-full items-start justify-center max-w-7xl mx-auto space-y-4 mb-12 mt-8">
     <hr className="w-full border-1 border-gray-400 dark:border-gray-500 mb-6" />
     <div className="w-full max-w-2xl grid grid-cols-1 sm:grid-cols-2 gap-4">
       <ExternalLink
@@ -32,6 +32,12 @@ const Footer = () => (
       </ExternalLink>
       <ExternalLink href="/contact" alt="twitter">
         Contact
+      </ExternalLink>
+      <ExternalLink href="/personal" alt="twitter">
+        Personal
+      </ExternalLink>
+      <ExternalLink href="https://eliasm.dev" alt="Elias">
+        Made by Elias
       </ExternalLink>
     </div>
   </footer>
