9c1ab7910230518fd69f9e903d22d1944880574f
Eliascm17 <Elias.cmoreno17@gmail.com>
2021-05-28 18:45:11 -0500

    debugging static file serving

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   images: {
     domains: ["*"],
   },
@@ -11,40 +8,4 @@ module.exports = {
     }
     return config;
   },
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/photos": { page: "/photos" },
-  //     "/film": { page: "/film" },
-  //     "/contact": { page: "/contact" },
-  //     "/photos/fashion": { page: "/photos/fashion" },
-  //     "/photos/fashion/shoot-1": {
-  //       page: "/photos/fashion/[slug]",
-  //       query: { slug: "shoot-1" },
-  //     },
-  //     "/photos/personal": { page: "/photos/personal" },
-  //     "/photos/personal/Caleb": {
-  //       page: "/photos/personal/[slug]",
-  //       query: { slug: "Caleb" },
-  //     },
-  //     "/photos/portraits": { page: "/photos/portraits" },
-  //     "/photos/portraits/Jess": {
-  //       page: "/photos/portraits/[slug]",
-  //       query: { slug: "Jess" },
-  //     },
-  //     "/photos/promos": { page: "/photos/promos" },
-  //     "/photos/promos/HEB": {
-  //       page: "/photos/promos/[slug]",
-  //       query: { slug: "HEB" },
-  //     },
-  //     "/photos/weddings": { page: "/photos/weddings" },
-  //     "/photos/weddings/Aliza & Chris": {
-  //       page: "/photos/weddings/[slug]",
-  //       query: { slug: "Aliza & Chris" },
-  //     },
-  //   };
-  // },
 };
